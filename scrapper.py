ef2065b583ff8a18580cb2980f307de045115c48
cyrilico <jpdamas15@gmail.com>
2019-09-21 17:53:40 +0100

    Add penalty goal detection

--- scrapper.py
+++ scrapper.py
@@ -24,7 +24,7 @@ def getLineup(team, matchSoup):
         current_player['yellows'] = [event.text.strip() for event in event_list if 'yellow_card' in event.select_one('img')['src']]
         current_player['reds'] = [event.text.strip() for event in event_list if 'red_card' in event.select_one('img')['src']]
         current_player['own_goals'] = [event.text.strip() for event in event_list if 'own_goal' in event.select_one('img')['src']]
-        current_player['goals'] = [event.text.strip() for event in event_list if 'goal' in event.select_one('img')['src'] and 'own_goal' not in event.select_one('img')['src']]
+        current_player['goals'] = [event.text.strip() for event in event_list if 'goal' in event.select_one('img')['src'] or 'penalty' in event.select_one('img')['src'] and 'own_goal' not in event.select_one('img')['src']]
         current_player['sub_off'] = [event.text.strip() for event in event_list if 'substitution_off' in event.select_one('img')['src']] 
         result.append(current_player)
     
@@ -42,7 +42,7 @@ def getSeason(seasonYear):
     script_tag.decompose()
     seasonSoup = bs4.BeautifulSoup(seasonSoup.renderContents(), 'html.parser')
 
-    for fixture in seasonSoup.select('div.fixres__item')[:1]: #TODO: Remove hardcoded cut
+    for fixture in seasonSoup.select('div.fixres__item')[4:5]: #TODO: Remove hardcoded cut
         print("URL: %s" % fixture.find('a')['href'])
         print("Home Team: {} {}".format(fixture.select_one('span.matches__participant--side1 span.swap-text__target').text, fixture.select('span.matches__teamscores-side')[0].text.strip()))
         print("Away Team: {} {}".format(fixture.select_one('span.matches__participant--side2 span.swap-text__target').text, fixture.select('span.matches__teamscores-side')[1].text.strip()))

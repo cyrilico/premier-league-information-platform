1962c94ec6761f8a15c3ff9a39af4148931b23b2
darksystem <diogo.rey@hotmail.com>
2019-10-02 23:35:36 +0100

    Add scraped 14-15, 15-16 data

--- replacelast.py
+++ replacelast.py
@@ -1,6 +1,10 @@
 import os
 
+<<<<<<< Updated upstream
 filename = '16-17.json'
+=======
+filename = '15-16.json'
+>>>>>>> Stashed changes
 with open(filename, 'rb+') as f:
     f.seek(-1, os.SEEK_END)
     f.truncate()

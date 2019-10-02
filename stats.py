7eaf6c922ea0bd117b44428991063f28092fee3e
cyrilico <jpdamas15@gmail.com>
2019-10-02 23:49:13 +0100

    Fix stats mess I think

--- stats.py
+++ stats.py
@@ -7,47 +7,46 @@ import seaborn as sns
 from functools import reduce
 
 games = []
-for key in ['14-15','15-16','16-17','17-18','18-19']:
+for key in ['18-19']: #['14-15','15-16','16-17','17-18','18-19']:
     games.extend(json.load(open('{}.json'.format(key))))
 
-print(len(games))
 goals = dict()
 cards = dict()
 subs = dict()
 
 for game in games:
-    for starter in game['home_team']['lineup']:
-        for goal in starter['goals']+starter['own_goals']:
-            goals[goal] = goals[goal] + 1 if goal in goals else 1
-        for card in starter['yellows']+starter['reds']:
-            cards[card] = cards[card] + 1 if card in cards else 1
-        if 'sub_on' in starter:
-            sub_min = starter['sub_on'][0]
-            subs[sub_min] = subs[sub_min] + 1 if sub_min in subs else 1
+    for starter_home in game['home_team']['lineup']:
+        for goal_1 in starter_home['goals']+starter_home['own_goals']:
+            goals[goal_1] = goals[goal_1] + 1 if goal_1 in goals else 1
+        # for card in starter_home['yellows']+starter_home['reds']:
+        #     cards[card] = cards[card] + 1 if card in cards else 1
+        # if 'sub_on' in starter_home and len(starter_home['sub_on']) > 0:
+        #     sub_min = starter_home['sub_on'][0]
+        #     subs[sub_min] = subs[sub_min] + 1 if sub_min in subs else 1
     for sub in game['home_team']['subs']:
-        for goal in sub['goals']+starter['own_goals']:
-            goals[goal] = goals[goal] + 1 if goal in goals else 1
-        for card in starter['yellows']+starter['reds']:
-            cards[card] = cards[card] + 1 if card in cards else 1
-        if 'sub_on' in starter:
-            sub_min = starter['sub_on'][0]
-            subs[sub_min] = subs[sub_min] + 1 if sub_min in subs else 1
-    for starter in game['away_team']['lineup']:
-        for goal in starter['goals']+starter['own_goals']:
-            goals[goal] = goals[goal] + 1 if goal in goals else 1
-        for card in starter['yellows']+starter['reds']:
-            cards[card] = cards[card] + 1 if card in cards else 1
-        if 'sub_on' in starter:
-            sub_min = starter['sub_on'][0]
-            subs[sub_min] = subs[sub_min] + 1 if sub_min in subs else 1
-    for sub in game['away_team']['subs']:
-        for goal in starter['goals']+starter['own_goals']:
-            goals[goal] = goals[goal] + 1 if goal in goals else 1
-        for card in starter['yellows']+starter['reds']:
-            cards[card] = cards[card] + 1 if card in cards else 1
-        if 'sub_on' in starter:
-            sub_min = starter['sub_on'][0]
-            subs[sub_min] = subs[sub_min] + 1 if sub_min in subs else 1
+        for goal_2 in sub['goals']+sub['own_goals']:
+            goals[goal_2] = goals[goal_2] + 1 if goal_2 in goals else 1
+        # for card in sub['yellows']+sub['reds']:
+        #     cards[card] = cards[card] + 1 if card in cards else 1
+        # if 'sub_on' in sub and len(sub['sub_on']) > 0:
+        #     sub_min = sub['sub_on'][0]
+        #     subs[sub_min] = subs[sub_min] + 1 if sub_min in subs else 1
+    for starter_away in game['away_team']['lineup']:
+        for goal_3 in starter_away['goals']+starter_away['own_goals']:
+            goals[goal_3] = goals[goal_3] + 1 if goal_3 in goals else 1
+        # for card in starter_away['yellows']+starter_away['reds']:
+        #     cards[card] = cards[card] + 1 if card in cards else 1
+        # if 'sub_on' in starter_away and len(starter_away['sub_on']) > 0:
+        #     sub_min = starter_away['sub_on'][0]
+        #     subs[sub_min] = subs[sub_min] + 1 if sub_min in subs else 1
+    for sub_away in game['away_team']['subs']:
+        for goal_4 in sub_away['goals']+sub_away['own_goals']:
+            goals[goal_4] = goals[goal_4] + 1 if goal_4 in goals else 1
+        # for card in sub_away['yellows']+sub_away['reds']:
+        #     cards[card] = cards[card] + 1 if card in cards else 1
+        # if 'sub_on' in sub_away and len(sub_away['sub_on']) > 0:
+        #     sub_min = sub_away['sub_on'][0]
+        #     subs[sub_min] = subs[sub_min] + 1 if sub_min in subs else 1
 
-print(sum(cards.values()))
+print(sum(goals.values()))
 

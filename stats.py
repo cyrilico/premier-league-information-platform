5497eed502fdf5e965b982002a30395cda16adb8
darksystem <diogo.rey@hotmail.com>
2019-10-09 21:43:36 +0100

    Updated Graph - Goals By Month/Season

--- stats.py
+++ stats.py
@@ -71,10 +71,10 @@ report_length_df = pd.DataFrame([{'season': k, 'report_lengths': v} for k,v in r
 final_df = final_df.set_index('minute')
 final_df = final_df.sort_index()
 final_df = final_df.transpose()
-final_df.T.plot()
-plt.xlabel('Minutes')
-plt.ylabel('Events')
-plt.title('Events by Minute')
+# final_df.T.plot()
+# plt.xlabel('Minutes')
+# plt.ylabel('Events')
+# plt.title('Events by Minute')
 
 data = pd.read_csv("gamesprocessed.csv")
 goals_by_season = [{'season': key, 'nr_goals': data[idx*380:(idx+1)*380]['HomeFTScore'].sum()+data[idx*380:(idx+1)*380]['AwayFTScore'].sum()} \
@@ -100,17 +100,17 @@ for idx, key in enumerate(['14-15', '15-16', '16-17', '17-18', '18-19']):
     #Uncomment if graphing purely goal count
     goals_by_month_season[key] = season_df_grouped['HomeFTScore'].sum()+season_df_grouped['AwayFTScore'].sum()
     #Uncomment if graphing goal count per number of games
-    goals_by_month_season[key] = (season_df_grouped['HomeFTScore'].sum()+season_df_grouped['AwayFTScore'].sum())/season_df_grouped['HomeFTScore'].count()
+    # goals_by_month_season[key] = (season_df_grouped['HomeFTScore'].sum()+season_df_grouped['AwayFTScore'].sum())/season_df_grouped['HomeFTScore'].count()
 
 # Graph 2
 goalsBySeason = pd.DataFrame(goals_by_season)
 goalsBySeason = goalsBySeason.set_index('season')
-goalsBySeason.plot.bar(legend=None)
-for i, v in enumerate(goalsBySeason.get('nr_goals')):
-    plt.text(i - 0.15, v + 5, str(v))
-plt.xlabel('Season')
-plt.ylabel('Goals')
-plt.title('Goals by Season')
+# goalsBySeason.plot.bar(legend=None)
+# for i, v in enumerate(goalsBySeason.get('nr_goals')):
+#     plt.text(i - 0.15, v + 5, str(v))
+# plt.xlabel('Season')
+# plt.ylabel('Goals')
+# plt.title('Goals by Season')
 
 
 # Graph 3
@@ -119,13 +119,21 @@ goalsByMonthSeason = goalsByMonthSeason.transpose()
 goalsByMonthSeason = goalsByMonthSeason[['08','09','10','11','12','01','02','03','04','05']]
 goalsByMonthSeason = goalsByMonthSeason.rename(columns=lambda x: calendar.month_abbr[int(x)])
 
-ax = goalsByMonthSeason.plot.bar(stacked=True, legend='reverse')
-handles, labels = ax.get_legend_handles_labels()
-plt.legend(handles=reversed(handles), labels=reversed(labels), title="Months", loc='center left', bbox_to_anchor=(1.0, 0.5))
+# ax = goalsByMonthSeason.plot.barh(stacked=True, legend='reverse')
+# handles, labels = ax.get_legend_handles_labels()
+# plt.legend(handles=reversed(handles), labels=reversed(labels), title="Months", loc='center left', bbox_to_anchor=(1.0, 0.5))
+# plt.title("Goals By Month/Season")
+# plt.ylabel("Goals")
+# plt.xlabel("Season")
+
+# Graph 4
+goalsByMonthSeason.T.plot()
+plt.legend(title="Seasons")
 plt.title("Goals By Month/Season")
 plt.ylabel("Goals")
-plt.xlabel("Season")
+plt.xlabel("Month")
 
+#Graph 5
 
 # Show Graphs
-#plt.show()
+plt.show()
